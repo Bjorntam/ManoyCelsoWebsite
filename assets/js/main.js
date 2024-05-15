4869c43a38997dd49624eb23d1793c8b92d12d91
Bjorno <ingemarbjorn.r@gmail.com>
2024-05-15 16:45:31 +0800

    updater

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,36 @@ toggle.classList.toggle('show-icon')
 }
 
 showMenu('nav-toggle','nav-menu')
+
+/*=============== Toggle Menu ===============*/
+
+const regular = "Regular Batos";
+
+function changer(page){
+       switch (page) {
+              case regular:
+              document.getElementById('pointer').style.left="156px";
+              changeItem("PANCIT BATO REGULAR","regular");break;
+              case siomai:
+              document.getElementById('pointer').style.left="320px";
+              changeItem("PANCIT BATO W/ SIOMAI","siomai");break;
+              case shanghai:
+              changeItem("PANCIT BATO W/ SHANGHAI","shanghai");break;
+              case dinuguan:
+              changeItem("PANCIT BATO W/ DINUGUAN","kawali");break;
+              case kawali:
+              changeItem("PANCIT BATO W/ LECHON KAWALI","bulaklak");break;
+              case bulaklak:
+              changeItem("PANCIT BATO W/ CHICHARON BULAKLAK","overload");break;
+              case overload:
+              changeItem("PANCIT BATO OVERLOAD");break;
+       }
+}
+function changeItem(name,forPointer) {
+       var title = document.getElementById('name');
+       
+       
+       document.getElementById('name').innerText = name;
+
+
+}
